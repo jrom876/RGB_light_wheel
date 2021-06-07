c82926a090ade9c4fe4b913748db1da2f7f7a220
jrom876 <jrom876@gmail.com>
2021-06-07 08:46:19 -0600

    Update RGB_light_wheel.ino

--- RGB_light_wheel/RGB_light_wheel.ino
+++ RGB_light_wheel/RGB_light_wheel.ino
@@ -3,30 +3,10 @@
   * 
   * A dimmable color spectrum generator for 
   * the PL9823 Programmable RGB LED
+  * 
+  * Jake Romero, Creative Engineering Solutions
   * CESLLC, August 2020
-  * 
-  * Jake Romero from Creative Engineering Solutions here: 
   *  
-  * When I first tinkered with the Knight Rider instructables 
-  * project for PL9823 Programmable RGB LEDs, I found that 
-  * at full power many brands of LEDs were way too bright for 
-  * the test bench and production environments. 
-  * 
-  * So I modified masteruan's code by adding a simple 6-bit attenuator 
-  * to the color function call, and a much shorter delay for the brand
-  * of LED I am using.
-  * 
-  * I also hard-coded options for evenly-spaced color transitions 
-  * and wrote for loops to iterate through them.
-  *
-  * Lastly, I wrote two white light dimmer functions just for fun.
-  * 
-  * @credits
-  * Thanks to masteruan at instructables.com and to the folks at 
-  * Adafruit for contributing the Adafruit_NeoPixel library. 
-  * 
-  * https://www.instructables.com/id/Multicolor-Knight-Rider-with-RGB-PL9823-LED-Arduin/
-  * https://github.com/adafruit/Adafruit_NeoPixel
 */
 #include <Adafruit_NeoPixel.h>
 
